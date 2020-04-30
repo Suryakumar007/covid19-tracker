e7a18e5b9a11d6156e373762ef6675f0f166f1ee
Suryakumar Arumugam <asdsurya@gmail.com>
2020-04-30 12:46:53 +0530

    Updated for dark mode

--- js/appcheck.js
+++ js/appcheck.js
@@ -329,3 +329,20 @@ function loadInd(){
         loadIndChart();
     }
 }
+
+document.addEventListener('DOMContentLoaded',function()
+{
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+});
